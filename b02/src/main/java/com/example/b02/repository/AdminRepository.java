97e33ee2c96ad9185d70aa9a7b1715d5595a6aff
ohhyunmin <ohhyunmin91@gmail.com>
2025-02-07 11:52:49 +0900

    Delete b02/src/main/java/com/example/b02/repository/AdminRepository.java

--- b02/src/main/java/com/example/b02/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.b02.repository;
-
-import com.example.b02.domain.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
