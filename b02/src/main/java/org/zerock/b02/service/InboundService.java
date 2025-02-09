7228de843df93336b04a633e86bfaa8cdcb93bae
JaehunKim-A <kimjaehun8988@gmail.com>
2025-02-09 14:27:39 +0900

    Add files via upload

---
+++ b02/src/main/java/org/zerock/b02/service/InboundService.java
@@ -0,0 +1,19 @@
+package org.zerock.b02.service;
+
+import org.zerock.b02.dto.InboundDTO;
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
+
+public interface InboundService {
+    // 등록
+    Long register(InboundDTO inboundDTO);
+    // 1행 조회
+    InboundDTO readOne(Long inboundId);
+    // 1행 수정
+    void modify(InboundDTO inboundDTO);
+    // 1행 삭제
+    void remove(Long inboundId);
+
+    PageResponseDTO list(PageRequestDTO pageRequestDTO);
+
+}
---
+++ b02/src/main/java/org/zerock/b02/service/ProductService.java
@@ -0,0 +1,18 @@
+package org.zerock.b02.service;
+
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
+import org.zerock.b02.dto.ProductDTO;
+
+public interface ProductService {
+    Long register(ProductDTO productDTO);
+
+    ProductDTO readOne(Long productId);
+
+    void modify(ProductDTO productDTO);
+
+    void remove(Long productId);
+
+    PageResponseDTO<ProductDTO> list(PageRequestDTO pageRequestDTO);
+
+}
