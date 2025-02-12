6547bbac4767e0a4ff15092a821f20cf74d27ae9
JaehunKim-A <kimjaehun8988@gmail.com>
2025-02-12 14:38:46 +0900

    Add files via upload

---
+++ src/main/java/org/zerock/b02/domain/Inbound.java
@@ -0,0 +1,60 @@
+package org.zerock.b02.domain;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@ToString
+@AllArgsConstructor
+@Getter
+@Builder
+@Table(name = "inbound")
+public class Inbound extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inboundId;
+
+    @Column(nullable = false, unique = true)
+    private String inboundCode;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "productCode", referencedColumnName = "productCode", nullable = false)
+    private Product product;
+
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "supplierId", referencedColumnName = "supplierId", nullable = false)
+    private Supplier supplier;
+
+    @Column(nullable = false)
+    private Long quantity;
+
+    @Column(nullable = false)
+    private LocalDateTime inboundDate;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(name = "status",nullable = false)
+    private String inboundStatus;
+
+    // 수정시 변경할것들
+    public void change(Product product,
+                       String inboundCode,
+                       Supplier supplier,
+                       Long quantity,
+                       LocalDateTime inboundDate,
+                       String description,
+                       String inboundStatus){
+        this.inboundCode = inboundCode;
+        this.product = product;
+        this.supplier = supplier;
+        this.quantity = quantity;
+        this.inboundDate = inboundDate;
+        this.description = description;
+        this.inboundStatus = inboundStatus;
+    }
+}
---
+++ src/main/java/org/zerock/b02/service/ProductServiceImpl.java
@@ -0,0 +1,93 @@
+package org.zerock.b02.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.zerock.b02.domain.Product;
+import org.zerock.b02.dto.PageRequestDTO;
+import org.zerock.b02.dto.PageResponseDTO;
+import org.zerock.b02.dto.ProductDTO;
+import org.zerock.b02.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+@Transactional
+public class ProductServiceImpl implements ProductService {
+    private final ModelMapper modelMapper;
+    private final ProductRepository productRepository;
+
+    @Override
+    public Long register(ProductDTO productDTO) {
+        Product product = modelMapper.map(productDTO, Product.class);
+        Long productId = productRepository.save(product).getProductId();
+        return productId;
+
+    }
+
+    @Override
+    public ProductDTO readOne(Long productId) {
+        Optional<Product> result = productRepository.findById(productId);
+        Product product = result.orElseThrow();
+        ProductDTO productDTO = modelMapper.map(product, ProductDTO.class);
+        return productDTO;
+
+    }
+
+    @Override
+    @Transactional
+    public void modify(ProductDTO productDTO) {
+        Optional<Product> result = productRepository.findById(productDTO.getProductId());
+        Product product = result.orElseThrow();
+        product.change(productDTO.getProductCode(),
+                productDTO.getProductName(),
+                productDTO.getProductType(),
+                productDTO.getProductWeight(),
+                productDTO.getProductSize());
+        productRepository.save(product);
+    }
+
+    @Override
+    public void remove(Long productId) {
+        productRepository.deleteById(productId);
+    }
+
+    @Override
+    public PageResponseDTO list(PageRequestDTO pageRequestDTO) {
+        String[] types = pageRequestDTO.getTypes();
+        String keyword = pageRequestDTO.getKeyword();
+        Pageable pageable = pageRequestDTO.getPageable("productId");
+
+        Page<Product> result = productRepository.searchAll(types, keyword, pageable);
+
+        List<ProductDTO> dtoList = result.getContent().stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+
+        return PageResponseDTO.<ProductDTO>builder()
+                .pageRequestDTO(pageRequestDTO)
+                .dtoList(dtoList)
+                .total((int) result.getTotalElements())
+                .build();
+    }
+
+    @Override
+    public List<ProductDTO> getAllProducts() {
+        List<Product> products = productRepository.findAll();  // 전체 데이터 가져오기
+
+        log.info("data" + products.size());
+
+        return products.stream()
+                .map(product -> modelMapper.map(product, ProductDTO.class))
+                .collect(Collectors.toList());
+    }
+
+}
