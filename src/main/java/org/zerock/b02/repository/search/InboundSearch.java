429f3cab644d686f8d99ac565e254d4af267f725
JaehunKim-A <kimjaehun8988@gmail.com>
2025-02-18 17:27:55 +0900

    Delete src directory

--- src/main/java/org/zerock/b02/repository/search/InboundSearch.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.b02.repository.search;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.zerock.b02.domain.Inbound;
-
-public interface InboundSearch {
-    Page<Inbound> searchAll(String[] types, String keyword, Pageable pageable);
-}
