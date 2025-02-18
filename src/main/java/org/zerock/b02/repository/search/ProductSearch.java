ac965ba22356b2ca471f2813fb12cb37083eb4d3
JaehunKim-A <kimjaehun8988@gmail.com>
2025-02-18 11:23:16 +0900

    Delete src directory

--- src/main/java/org/zerock/b02/repository/search/ProductSearch.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.b02.repository.search;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.zerock.b02.domain.Product;
-
-public interface ProductSearch {
-	Page<Product> searchAll(String[] types, String keyword, Pageable pageable);
-}
