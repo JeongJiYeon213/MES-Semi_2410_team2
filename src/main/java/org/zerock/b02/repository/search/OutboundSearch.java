45a9637bc41c0621f84a309f1ae4ffa385e2bdf8
JaehunKim-A <kimjaehun8988@gmail.com>
2025-02-18 09:27:07 +0900

    Delete src directory

--- src/main/java/org/zerock/b02/repository/search/OutboundSearch.java
+++
@@ -1,9 +0,0 @@
-package org.zerock.b02.repository.search;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.zerock.b02.domain.Outbound;
-
-public interface OutboundSearch {
-    Page<Outbound> searchAll(String[] types, String keyword, Pageable pageable);
-}
