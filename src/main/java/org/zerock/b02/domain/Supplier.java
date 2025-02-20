af66b062e4330a934334045a69a9615659ecb9f4
ohhyunmin <ohhyunmin91@gmail.com>
2025-02-20 11:36:21 +0900

    Add files via upload

--- src/main/java/org/zerock/b02/domain/Supplier.java
+++ src/main/java/org/zerock/b02/domain/Supplier.java
@@ -3,7 +3,7 @@ package org.zerock.b02.domain;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.UUID;
+import java.io.Serializable;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Entity
@@ -12,17 +12,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
-public class Supplier extends BaseEntity {
+public class Supplier extends BaseEntity implements Serializable {
 
     @Id
     @Column(length = 50, nullable = false)
     private String supplierId;
     @Column(length = 50, nullable = false)
     private String supplierName;
-    @Column(length = 100, nullable = false)
+    @Column(length = 50, nullable = false)
     private String supplierInfo;
 
-    private static final AtomicInteger supplierIdCounter = new AtomicInteger(1);
+    private static final AtomicInteger customerIdCounter = new AtomicInteger(1);
 
     @PrePersist
     public synchronized void prePersist() {
@@ -35,12 +35,10 @@ public class Supplier extends BaseEntity {
         return "company" + generateRandomAlphaNumeric(4);
     }
 
-    // 4자리의 랜덤 알파벳 또는 숫자를 생성하는 메서드
     private String generateRandomAlphaNumeric(int length) {
         String characters = "ABCDEFGH0123456789";
         StringBuilder sb = new StringBuilder(length);
 
-        // 4자리 랜덤 문자 생성
         for (int i = 0; i < length; i++) {
             int index = (int) (Math.random() * characters.length());
             sb.append(characters.charAt(index));
@@ -49,7 +47,7 @@ public class Supplier extends BaseEntity {
         return sb.toString();
     }
 
-    public void change(String supplierId, String supplierName, String supplierInfo){
+    public void change(String supplierId, String supplierName, String supplierInfo) {
         this.supplierId = supplierId;
         this.supplierName = supplierName;
         this.supplierInfo = supplierInfo;
